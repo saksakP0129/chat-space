ebdc68251ef3cb6cd06563c54df6a9a4d79a60fb
saksakP0129 <ametoyuki0712@gmail.com>
2020-03-13 20:21:04 +0900

    非同期通信実装

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -51,11 +51,13 @@ $('#new_message').on('submit', function(e){
     .done(function(data){
         console.log("test");
         var html = buildHTML(data);
+    })
+    .always(function(){
         $('.messages').append(html);      
         $('form')[0].reset();
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
         $(".message--send").prop('disabled', false);
-    })
+    })    
     .fail(function() {
         alert("メッセージ送信に失敗しました");
     })
